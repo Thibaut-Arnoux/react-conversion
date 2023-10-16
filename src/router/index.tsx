ac0b7376cb771e7db259364d553c1470d5097ce8
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-10-16 22:28:46 +0200

    [Router] : init routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,10 @@
 import { NavBar } from '@/components/ui/NavBar';
+import { ForgotPassword } from '@/pages/Auth/ForgotPassword';
+import { Login } from '@/pages/Auth/Login';
+import { Register } from '@/pages/Auth/Register';
+import { ResetPassword } from '@/pages/Auth/ResetPassword';
 import Home from '@/pages/Home';
+import { Profile } from '@/pages/Profile';
 import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
@@ -11,8 +16,24 @@ const router = createBrowserRouter([
                 element: <Home />
             },
             {
-                path: '/home',
-                element: <>This is fine</>
+                path: '/register',
+                element: <Register />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/forgot-password',
+                element: <ForgotPassword />
+            },
+            {
+                path: '/reset-password',
+                element: <ResetPassword />
+            },
+            {
+                path: '/profile',
+                element: <Profile />
             }
         ]
     }
