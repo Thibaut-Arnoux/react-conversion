5c74002ce6b35079f9d7ebb7868b2b3d89a9b3db
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-10-17 17:21:33 +0200

    fix: Static analyse

--- src/pages/Auth/ResetPassword/index.tsx
+++ src/pages/Auth/ResetPassword/index.tsx
@@ -1,7 +1,7 @@
 import { useSearchParams } from 'react-router-dom';
 
 export const ResetPassword = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const token = searchParams.get('token');
     const email = searchParams.get('email');
     return (
