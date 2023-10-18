989698ab08ce04aa4b7c486f7b9e5859f212f144
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-10-18 15:24:25 +0200

    fix: Load trad from front and not from back

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,21 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+import en from '../public/locales/en/translation.json';
+import fr from '../public/locales/fr/translation.json';
 
 void i18n
-    .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        resources: {
+            en: {
+                translation: en
+            },
+            fr: {
+                translation: fr
+            }
+        },
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false
