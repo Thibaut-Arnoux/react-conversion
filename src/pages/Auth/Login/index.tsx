d960e297ea6e587fb11c82bb5373f77bd0d7dc1f
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-10-18 15:27:41 +0200

    feat: Use login form inside login page

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,3 +1,18 @@
+import { LoginForm } from '@/components/form/LoginForm';
+import { CenteredBox } from '@/components/layout/CenteredBox';
+
 export const Login = () => {
-    return <>Login</>;
+    return (
+        <CenteredBox
+            sx={{
+                backgroundColor: 'white',
+                width: '50%',
+                maxWidth: '500px',
+                padding: '20px',
+                borderRadius: '10px'
+            }}
+        >
+            <LoginForm />
+        </CenteredBox>
+    );
 };
